1e71e3daf6b8af8a5105ddae57af823e6b6cc2ae
matheus Grundler <matheusgrundler@gmail.com>
2020-05-12 16:09:35 -0300

    att

---
+++ build/precache-manifest.39d532ac234f36d252cdcc0dc86da215.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eef7ad6c6ebcca34a5f65b227d52d93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a37a61a54321e298175",
+    "url": "/static/js/2.b27f7568.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b27f7568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cee81198369aac27d3e",
+    "url": "/static/js/main.5cc21b5f.chunk.js"
+  },
+  {
+    "revision": "11fb105c6e1b74d23761",
+    "url": "/static/js/runtime-main.35c695ee.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/static/media/github_background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/static/media/logo.04f6798f.svg"
+  }
+]);
